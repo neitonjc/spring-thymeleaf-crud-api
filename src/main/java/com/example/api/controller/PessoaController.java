501227f20207110fc4ac905ba5c4fb7f37272a1c
neitonjc <78852153+neitonjc@users.noreply.github.com>
2023-03-28 16:22:54 -0300

    Update PessoaController.java

--- src/main/java/com/example/api/controller/PessoaController.java
+++ src/main/java/com/example/api/controller/PessoaController.java
@@ -1,4 +1,4 @@
-package com.example.api.cntroller;
+package com.example.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
